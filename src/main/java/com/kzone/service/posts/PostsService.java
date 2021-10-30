b28db47e39f3484915d65c7d389d3933914bf6c8
97Kzone <mmnm3@naver.com>
2021-10-30 14:07:07 +0900

    21.10.29

--- src/main/java/com/kzone/service/posts/PostsService.java
+++ src/main/java/com/kzone/service/posts/PostsService.java
@@ -3,12 +3,15 @@ package com.kzone.service.posts;
 
 import com.kzone.domain.posts.Posts;
 import com.kzone.domain.posts.PostsRepository;
+import com.kzone.web.dto.PostsListResponseDto;
 import com.kzone.web.dto.PostsResponseDto;
 import com.kzone.web.dto.PostsSaveRequestDto;
 import com.kzone.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +40,17 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public Object findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
 }
